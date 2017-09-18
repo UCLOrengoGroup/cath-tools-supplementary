04d4e36d40edc3a6f918d775a85967aa914765ce
Tony Lewis <tonyelewis@users.noreply.github.com>
2017-09-18 11:43:10 +0100

    Tweak: whitespace

--- generate_changelog/generate_changelog.pl
+++ generate_changelog/generate_changelog.pl
@@ -3,21 +3,21 @@
 use strict;
 use warnings;
 
-use Carp          qw/ confess        /; 
+use Carp          qw/ confess        /;
 use Data::Dumper;
-use English       qw/ -no_match_vars /; 
+use English       qw/ -no_match_vars /;
 use feature       qw/ say            /;
-use JSON          qw/ decode_json    /; 
-use LWP::Simple; 
-my $builds_url      = 'https://api.github.com/repos/UCLOrengoGroup/cath-tools/releases'; 
+use JSON          qw/ decode_json    /;
+use LWP::Simple;
+my $builds_url      = 'https://api.github.com/repos/UCLOrengoGroup/cath-tools/releases';
 
 # http://keepachangelog.com/en/0.3.0/
 
-my $json         = get( $builds_url ); 
-if ( ! defined( $json ) ) { 
-	confess "Couldn't get \"$builds_url\" : $OS_ERROR"; 
+my $json         = get( $builds_url );
+if ( ! defined( $json ) ) {
+	confess "Couldn't get \"$builds_url\" : $OS_ERROR";
 } 
-my $releases = decode_json( $json ); 
+my $releases = decode_json( $json );
 
 say "# Summary of changes in cath-tools releases\n\n";
 
@@ -32,7 +32,7 @@ print join(
 			my $published_at = $release->{ published_at };
 
 			$published_at =~ s/T\d{2}:\d{2}:\d{2}Z//g;
-			$body =~ s/\b(\w{40})\b/[$1](https:\/\/github.com\/UCLOrengoGroup\/cath-tools\/commit\/$1)/g; 
+			$body =~ s/\b(\w{40})\b/[$1](https:\/\/github.com\/UCLOrengoGroup\/cath-tools\/commit\/$1)/g;
 			$body =~ s/#(\d+)\b/[$1](https:\/\/github.com\/UCLOrengoGroup\/cath-tools\/issues\/$1)/g ;
 
 			  "### [$tag_name]($html_url) $name\n\n"
